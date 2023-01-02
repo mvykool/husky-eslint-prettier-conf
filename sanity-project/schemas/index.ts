6a1ceeef637ab4c58593c042b3d167dc41710bc4
maicol <maykolandres4@gmail.com>
2023-01-02 06:23:43 -0500

    added schemas

--- sanity-project/schemas/index.ts
+++ sanity-project/schemas/index.ts
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import post from './post'
+import comment from './comment'
+import user from './user'
+import postedBy from './postedBy'
+
+export const schemaTypes = [post, postedBy, comment, user]
