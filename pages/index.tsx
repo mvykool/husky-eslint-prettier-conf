5be40a41c94584c65ce365503807d4c543d138d4
maicol <maykolandres4@gmail.com>
2023-01-02 06:52:24 -0500

    added gradient

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,9 +10,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main >
-        <div > 
+        <div className='bg-[var(--bg-dark)] w-screen h-screen' > 
 
-          <h1 className='text-green-500'>hola</h1>
+          <h1 className='gradient text-white font-bold w-20 p-4'>hola</h1>
      
         </div>
       </main>
