f75e3fdc1cd4f10c63bb186753d20e82cf99b466
maicol <maykolandres4@gmail.com>
2023-01-03 04:05:18 -0500

    added more stuff

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import { TiThMenu} from 'react-icons/ti'
 
 const Navbar = () => {
   return (
-    <div className='h-10 bg-[var(--bg-nav)] fixed w-full navbar '>
+    <div className='h-14 bg-[var(--bg-nav)] fixed w-full navbar flex justify-around items-center'>
       {/**logo */}
-       hola
-
+       <h1 className='gradient-text'>Descry</h1>
       {/**search */}
-
+       <div>
+        <form>
+          <input 
+          type="search"
+          placeholder='search'
+          className='px-2 rounded-sm outline-none' />
+          <button type='submit'></button>
+        </form>
+       </div>
 
       {/**menu and more */}
+
+      <div>
+        <TiThMenu className='h-6 w-6 text-[var(--gradient)]'/>
+      </div>
     </div>
   )
 }
