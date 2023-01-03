72e69de52302225019c07d6bf20c7b5fa3c85fa1
maicol <maykolandres4@gmail.com>
2023-01-03 03:51:45 -0500

    added gradient text

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -4,10 +4,13 @@ import { topics } from '../utils/topics'
 const Categories = () => {
   return (
     <div>
-      <div className='flex justify-center space-x-5 pt-5 overflow-x-auto'>
+      <div className='pt-16 ml-5 gradient-text font-semibold'>
+        <h3>Categories</h3>
+      </div>
+      <div className='flex justify-center space-x-5 pt-5 pl-20 pb-6 items-center overflow-x-auto'>
         {topics.map((topic) => (
-            <div key={topic.name} className='gradient p-2 rounded-md text-white'>
-               {topic.name}
+            <div key={topic.name} className='gradient p-1 rounded-md text-white'>
+               <p className='w-[30vw] flex justify-center'>{topic.name}</p>
             </div>
         ))}
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,9 @@ export default function Home({posts}: Props) {
 
         {/**categories */}
 
-        <Categories/>
+       <div className='w-[95vw] mx-auto'>
+       <Categories/>
+       </div>
 
         {/**feed */}
 
